32a3a87d93ae80de3ee79aada61902207ef7bc57
Mears <jordan@vireocreative.ca>
2021-04-28 17:51:52 -0600

    Adds some markup

--- pages/the-book.js
+++ pages/the-book.js
@@ -1,18 +1,37 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 const TheBook = () => {
   return (
     <Layout>
       <div className="container">
-        <h1>The Book,<br /> The Legend.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in nibh rutrum nisl accumsan iaculis. Sed euismod enim ac eleifend lacinia. Donec consequat, dui ut dapibus dapibus, turpis lacus euismod nunc, in commodo mauris turpis malesuada turpis. Suspendisse consequat augue at urna mattis, non lacinia nunc molestie. Nullam dapibus neque id enim pulvinar placerat. Nullam a odio vulputate odio tristique cursus nec eu dolor. Vestibulum dignissim condimentum velit, at rutrum turpis ultricies sit amet. Pellentesque ultricies elit odio. Nam laoreet rhoncus mauris et mollis. Maecenas ut dui molestie, malesuada metus sed, laoreet lacus.</p>
-        <p>Proin ultricies auctor libero quis sagittis. Morbi rhoncus mi efficitur blandit aliquam. Maecenas blandit lacinia faucibus. Proin urna lacus, condimentum sit amet nisl in, congue vulputate nunc. Fusce arcu massa, fermentum nec aliquet vitae, dictum eu leo. Duis pulvinar est leo, faucibus blandit urna congue sodales. Mauris luctus congue enim, ac pulvinar mi egestas facilisis. Fusce commodo tellus tortor, nec placerat sem tempus et. Duis faucibus justo nisl, eu faucibus est cursus non. Integer lobortis commodo rhoncus. Morbi sed vulputate nisi. Nulla commodo feugiat turpis.</p>
-        <h2>Proin ultricies auctor libero quis sagittis.</h2>
-        <p>Proin ultricies auctor libero quis sagittis. Morbi rhoncus mi efficitur blandit aliquam. Maecenas blandit lacinia faucibus. Proin urna lacus, condimentum sit amet nisl in, congue vulputate nunc. Fusce arcu massa, fermentum nec aliquet vitae, dictum eu leo. Duis pulvinar est leo, faucibus blandit urna congue sodales. Mauris luctus congue enim, ac pulvinar mi egestas facilisis. Fusce commodo tellus tortor, nec placerat sem tempus et. Duis faucibus justo nisl, eu faucibus est cursus non. Integer lobortis commodo rhoncus. Morbi sed vulputate nisi. Nulla commodo feugiat turpis.</p>
-        <h3>Proin ultricies auctor libero quis sagittis.</h3>
-        <p>Proin ultricies auctor libero quis sagittis. Morbi rhoncus mi efficitur blandit aliquam. Maecenas blandit lacinia faucibus. Proin urna lacus, condimentum sit amet nisl in, congue vulputate nunc. Fusce arcu massa, fermentum nec aliquet vitae, dictum eu leo. Duis pulvinar est leo, faucibus blandit urna congue sodales. Mauris luctus congue enim, ac pulvinar mi egestas facilisis. Fusce commodo tellus tortor, nec placerat sem tempus et. Duis faucibus justo nisl, eu faucibus est cursus non. Integer lobortis commodo rhoncus. Morbi sed vulputate nisi. Nulla commodo feugiat turpis.</p>
         
+        <div className="">
+          <div className="">
+            <h1>The Book,<br /> The Legend.</h1>
+            <p>There were too many words in this old notebook that I enjoyed, and the book would have otherwise sat on the shelf forever, unread.</p>
+            <p>So I printed it.</p>
+            <p>And I gave it a name.</p>
+            <p>But that name was taken,<br />
+            so I called it something else.</p>
+            <p>It is on amazon, <br />
+            or I have copies of it.</p>
+              
+            <div className="buttons">
+              <a href="#/" className="button">Paperback</a>
+              <a href="#/" className="button">eBook</a>
+            </div>
+          </div>
+
+          <Image
+            src="/sparks-from-the-dark-cover.jpg"
+            alt="Sparks from the dark book cover"
+            height="1612px"
+            width="1080px"
+          />
         </div>
+
+      </div>
     </Layout>
   )
 }
